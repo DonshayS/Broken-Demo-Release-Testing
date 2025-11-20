aa7eedc0956fb1c8a1f9f9e30656836aff90c5fe
DonshayS <145791919+DonshayS@users.noreply.github.com>
2025-11-19 19:46:13 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+const message = (process.env.MESSAGE || "Hello World").toUpperCase(); 
+
+app.get('/', (req, res) => {
+    res.send(`Message: ${message}`);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
